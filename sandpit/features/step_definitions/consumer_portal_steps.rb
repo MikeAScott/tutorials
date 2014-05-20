4d210d2c24883bcf2ef0f3ea335a3613922ee0b5
mikeascott <mike.scott@sqs.com>
2014-05-20 18:36:27 +0100

    Sandpit: Added tests for user sign in Refactor to extract sign in page object

--- sandpit/features/step_definitions/consumer_portal_steps.rb
+++ sandpit/features/step_definitions/consumer_portal_steps.rb
@@ -1,10 +1,17 @@
-Given(/^I am on the Consumer Portal Home Page$/) do
+require 'sign_in_page'
+
+def goto_consumer_portal_page
   user_id = "songbird"
   password = "Inn0vat3"
   page_url = "os-bqa1.analoganalytics.com/consumer"
   visit "https://#{user_id}:#{password}@#{page_url}"
   
   page.title.should include("bespoke offers")
+end
+
+
+Given(/^I am on the Consumer Portal Home Page$/) do
+  goto_consumer_portal_page
 end
 
 When(/^I click "(.*?)"$/) do |link|
@@ -14,3 +21,32 @@ end
 Then(/^I expect to see the "(.*?)" form$/) do |text|
   expect(page).to have_content(text)
 end
+
+Given(/^I am on the user login page$/) do
+  goto_consumer_portal_page
+  click_on "Sign In"
+  @sign_in_page = SignInPage.new
+end
+
+When(/^I enter email "(.*?)" with password "(.*?)"$/) do |email, password|
+  @sign_in_page.fill_in_details(email, password)
+end
+
+Then(/^I expect to be signed in$/) do
+  expect(page).to have_content("You've signed in successfully.")
+end
+
+When(/^I click the sign in button$/) do
+  @sign_in_page.sign_in
+end
+
+Then(/^I expect to see error message: (.*)$/) do |error_message|
+  @sign_in_page.expect_error error_message
+end
+
+When(/^I attempt to sign in with email: "(.*?)" and password: "(.*?)"$/) do |email, password|
+  @sign_in_page.fill_in_details(email, password)
+  @sign_in_page.sign_in
+end
+
+
--- sandpit/features/support/env.rb
+++ sandpit/features/support/env.rb
@@ -2,3 +2,5 @@ require 'rspec/expectations'
 require 'selenium/webdriver'
 require 'capybara/cucumber'
 
+require 'page_helpers'
+
